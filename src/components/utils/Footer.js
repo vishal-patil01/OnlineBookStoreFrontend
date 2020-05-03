7610301004826a6ca6e682150b0a54bd1f06a132
mlatekar <mlatekar@gmail.com>
2020-05-03 15:06:41 +0530

    Added : Header And Footer Components

---
+++ src/components/utils/Footer.js
@@ -0,0 +1,20 @@
+import Typography from "@material-ui/core/Typography";
+import React, {Component} from "react";
+import {StylesProvider} from "@material-ui/core/styles";
+
+export default class Footer extends Component {
+    render() {
+        return (
+            <StylesProvider injectFirst>
+                <footer className="App-footer">
+                    <Typography className="footer-font">
+                        &copy; Copyright {new Date().getFullYear()} - {new Date().getFullYear() + 1} , The Book Store
+                        Pvt
+                        Ltd.
+                    </Typography>
+                </footer>
+            </StylesProvider>
+        );
+    }
+}
+
